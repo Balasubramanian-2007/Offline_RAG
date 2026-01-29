63a2e1952626b17c64b709ec61220fa40eb78e54
Balasubramanian <subramaniang172@gmail.com>
2026-01-29 16:58:43 +0530

    Sentence tranformers included

--- text_embedding.py
+++ text_embedding.py
@@ -4,7 +4,19 @@ import re
 import docx
 from collections import Counter
 import psycopg2
+from sentence_transformers import SentenceTransformer
+import faiss
+from typing import List
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException
 
+
+
+#Embedding model:
+'''
+URL for hugging face sentence transformers =
+https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
+'''
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
 # DB CONNECTION :
 conn=psycopg2.connect(
@@ -34,63 +46,7 @@ def isheading(line):
     if line.lower().startswith(("the ", "a ", "an ","and ")): score -= 2
 
     return score if score >= threshold else False
-'''
-def extract_text_from_pdf(file_name):
-    reader = PdfReader(file_name)
 
-    sections = []
-    current_section = None
-    previousHeadingScore = 0
-
-    for page_no, page in enumerate(reader.pages, start=1):
-        text = page.extract_text()
-        if not text:
-            continue
-
-        lines = text.split("\n")
-
-        for line in lines:
-            cleanLine = line.strip()
-            if not cleanLine:
-                continue
-
-            heading_score = isheading(cleanLine)
-
-            if heading_score and (heading_score > previousHeadingScore or heading_score < previousHeadingScore):
-                # close previous section
-                if current_section:
-                    sections.append(current_section)
-
-                current_section = {
-                    "heading": cleanLine,
-                    "content": "",
-                    "document_name": file_name,
-                    "page_start": page_no,
-                    "page_end": page_no
-                }
-                previousHeadingScore = heading_score
-            else:
-                if not current_section:
-                    current_section = {
-                        "heading": "GENERAL",
-                        "content": "",
-                        "document_name": file_name,
-                        "page_start": page_no,
-                        "page_end": page_no
-                    }
-
-                current_section["content"] += " " + cleanLine
-                current_section["page_end"] = page_no
-
-    if current_section:
-        sections.append(current_section)
-
-    # return sections
-        for i in sections:
-            print(i)
-            print("\n")
-
-'''
 
 def extract_text_from_pdf(file_name):
     reader = PdfReader(file_name)
@@ -154,56 +110,6 @@ def extract_text_from_pdf(file_name):
 
     return sections
 
-'''
-def extract_text_from_word(file_name):
-    document = docx.Document(file_name)
-
-    sections = []
-    current_section = None
-    previousHeadingScore = 0
-
-    for para_index, para in enumerate(document.paragraphs):
-        cleanLine = para.text.strip()
-        if not cleanLine:
-            continue
-
-        heading_score = isheading(cleanLine)
-
-        # if heading_score or heading_score > previousHeadingScore:
-        if heading_score :
-            if current_section:
-                sections.append(current_section)
-
-            current_section = {
-                "heading": cleanLine,
-                "content": "",
-                "document_name": file_name,
-                "para_start": para_index,
-                "para_end": para_index
-            }
-            previousHeadingScore = heading_score
-        else:
-            if not current_section:
-                current_section = {
-                    "heading": "GENERAL",
-                    "content": "",
-                    "document_name": file_name,
-                    "para_start": para_index,
-                    "para_end": para_index
-                }
-
-            current_section["content"] += " " + cleanLine
-            current_section["para_end"] = para_index
-
-    if current_section:
-        sections.append(current_section)
-
-    # return sections
-    for i in sections:
-        print(i)
-        print("\n")
-'''
-
 
 def extract_text_from_word(file_name):
     document = docx.Document(file_name)
@@ -261,7 +167,17 @@ def extract_text_from_word(file_name):
 
     return sections
 
+def file_uploads(files: List[UploadFile] = File(...)):
+    sentences="I am sudalai muthu !"
+    embeddings = model.encode(sentences)
+    print(embeddings)
+    for file in files:
+        if not file.endswith(".pdf") or not file.endswith(".docx"):
+            raise HTTPException(status_code=404,detail="Invalid File Type ! Upload a valid file PDF or Docx")
+        
 
-extract_text_from_word("D:\Academics Till Now\\3-rd SEM Academics\CN_Lab\CN_lab1_revised.docx")
+
+# extract_text_from_word("D:\Academics Till Now\\3-rd SEM Academics\CN_Lab\CN_lab1_revised.docx")
 # chunks=extract_text_from_pdf("documents\\40_a.pdf")
+# file_uploads()
 
