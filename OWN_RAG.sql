66915d4b4746e2624e27b321aa262912c5b51b47
Balasubramanian <subramaniang172@gmail.com>
2026-02-01 23:54:50 +0530

    Small changes in DB for incremental indexing

--- OWN_RAG.sql
+++ OWN_RAG.sql
@@ -8,3 +8,14 @@ CREATE TABLE chunks(
     pg_end INT,
     created_at TIMESTAMP DEFAULT NOW()
 )
+
+CREATE TABLE chunks(
+    chunk_id SERIAL PRIMARY KEY,
+    source_name TEXT,
+    heading TEXT,
+    content TEXT,
+    pg_start INT,
+    pg_end INT,
+    created_at TIMESTAMP DEFAULT NOW(),
+	deleted_at TIMESTAMP NULL
+)
