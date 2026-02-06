ccc6ad405b0f4362d1b704c91be129eb50e9dfb9
Balasubramanian <subramaniang172@gmail.com>
2026-02-06 19:01:09 +0530

    Updated DB schema

--- OWN_RAG.sql
+++ OWN_RAG.sql
@@ -1,13 +1,4 @@
 -- DB NAME in postgres : OWN_RAG
-CREATE TABLE chunks(
-    chunk_id SERIAL PRIMARY KEY,
-    source_name TEXT,
-    heading TEXT,
-    content TEXT,
-    pg_start INT,
-    pg_end INT,
-    created_at TIMESTAMP DEFAULT NOW()
-)
 
 CREATE TABLE chunks(
     chunk_id SERIAL PRIMARY KEY,
